0118b57bc3266213b7608c80a8283e6d9ad9f760
Prachiti Parab <prachitiparab275@gmail.com>
2025-08-07 18:58:12 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Fallback: send index.html for any route not handled above (like React Router)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', '/index.html'));
 });
 
 
